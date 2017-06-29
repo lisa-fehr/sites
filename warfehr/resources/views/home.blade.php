de35e641696ad4d293cfb30c9484942963d831ee
Lisa Fehr <lisa@warfehr.com>
2017-06-29 03:18:15 -0400

    Added linkedin link and resume pdf

--- warfehr/resources/views/home.blade.php
+++ warfehr/resources/views/home.blade.php
@@ -5,6 +5,7 @@
 <div class="box">
   <div class="profile vcard">
     <h1 class="fn">Lisa Fehr</h1>
+    <p><a href="/LisaFehr-resume.pdf"><span id="pdf-icon" class="icon"></span>Resume</a></p>
     <p><a href="mailto:lisa@warfehr.com" class="email">lisa@warfehr.com</a></p>
 
     <p class="adr"><span class="locality">Kitchener</span>, <span class="region">ON</span></p>
@@ -17,6 +18,8 @@
       <a href="https://twitter.com/lisa_warfehr" class="url"><span id="twitter-icon" class="icon"></span>lisa_warfehr</a>
       <br class="clear" />
       <a href="https://github.com/lisa-fehr" class="url"><span id="github-icon" class="icon"></span>lisa-fehr</a>
+      <br class="clear" />
+      <a href="https://www.linkedin.com/in/lisafehr/" class="url"><span id="linkedin-icon" class="icon"></span>lisafehr</a>
     </p>
   </div>
 </div>
